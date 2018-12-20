03c86044e7aaa41a71fd40ff5c9e45d3ee7f7641
Dominic <domyoung424@gmail.com>
2018-12-19 19:46:14 -0600

    allow heroku to choose port

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/doms/go-moji/emoji"
 )
+
+func getPort() string {
+	p := os.Getenv("PORT")
+	if p != "" {
+		return ":" + p
+	}
+	return ":8080"
+}
 
 func main() {
 	// serve stuff from static
@@ -15,6 +24,5 @@ func main() {
 	http.HandleFunc("/fetch-skin-tones", emoji.FetchSkinTonesHandler)
 
 	// listen on port
-	log.Println("Listening on :4567...")
-	log.Fatal(http.ListenAndServe(":4567", nil))
+	log.Fatal(http.ListenAndServe(getPort(), nil))
 }
